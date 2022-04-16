d3514f713932db35b4fb8f27a36b640f3d8bef0f
Rogozhin-Dmitry <62459927+Rogozhin-Dmitry@users.noreply.github.com>
2022-04-16 15:43:32 +0300

    Sixth lesson (remove view class, and make a view function)

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import item_list, ItemDetail
+from .views import item_detail, item_list
 
 urlpatterns = [
     path("", item_list, name='item_list'),
-    path("<int:item_num>/", ItemDetail.as_view(), name='item_detail'),
+    path("<int:item_num>/", item_detail, name='item_detail'),
 ]
--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -2,7 +2,6 @@ from django import forms
 from django.db.models import Avg, Count
 from django.http import HttpResponseNotFound
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import View
 from rating.models import Rating
 
 from catalog.models import Item
@@ -23,41 +22,31 @@ def item_list(request):
     return render(request, TEMPLATE, context)
 
 
-class ItemDetail(View):
+def item_detail(request, item_num):
     TEMPLATE = "catalog/item_detail.html"
-
-    def standard(self, request, item_num):
-        item = get_object_or_404(
-            Item.objects.published_items_with_category_name(),
-            id=item_num,
-        )
-        rating = Rating.objects.get_rating_form_item_id(item_num).aggregate(
-            Avg('star'), Count('star')
-        )
-        if request.user.is_authenticated:
-            user_rating = Rating.objects.get_rating_form_user_id_and_item_id(
-                request.user.id, item.id
-            )
-            if user_rating:
-                user_rating = user_rating.star
-        else:
-            user_rating = 0
-        form = RatingForm(request.POST or None)
-        return {
-            "item": item,
-            "rating": rating,
-            'user_rating': user_rating,
-            'form': form,
-        }
-
-    def get(self, request, item_num):
-        context = self.standard(request, item_num)
-        return render(request, self.TEMPLATE, context)
-
-    def post(self, request, item_num):
+    item = get_object_or_404(
+        Item.objects.published_items_with_category_name(),
+        id=item_num,
+    )
+    rating = Rating.objects.get_rating_form_item_id(item_num).aggregate(
+        Avg('star'), Count('star')
+    )
+    all_ratings = (
+        Rating.objects.select_related("item")
+        .only('item__id')
+        .filter(item__id=item_num)
+        .all()
+    )
+    form = RatingForm(request.POST or None)
+    context = {
+        "item": item,
+        "rating": rating,
+        'all_ratings': all_ratings,
+        'form': form,
+    }
+    if request.method == 'POST':
         if not request.user.is_authenticated:
             return HttpResponseNotFound()
-        context = self.standard(request, item_num)
         form = context['form']
         if form and form.is_valid():
             star = form.cleaned_data['star']
@@ -70,4 +59,5 @@ class ItemDetail(View):
                 defaults={'star': star},
             )
             return redirect('item_detail', item_num=context['item'].pk)
-        return render(request, self.TEMPLATE, context)
+        return render(request, TEMPLATE, context)
+    return render(request, TEMPLATE, context)
